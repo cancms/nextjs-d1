c5a7f88b291f74630408a6bc68c3479e6302fb89
cancms <cancms@163.com>
2025-02-25 15:59:10 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 const nextConfig = {
   output: 'export',  // Enables static HTML export
   trailingSlash: true, // Ensures correct path handling
-  assetPrefix: '/',   // Use relative paths for assets
+  // assetPrefix: '/',   // Use relative paths for assets
+  assetPrefix: 'nextjs-d1/',   // Use relative paths for assets
+
+  // env: {
+  //   BASE_URL: process.env.BASE_URL,
+  // },
+
 };
 
 export default nextConfig;
